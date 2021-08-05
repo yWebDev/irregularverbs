42f2bcc88833482171ddbac03e248327614fc4e9
Yurii Shpakovych <yurii.shpakovych@davita.com>
2021-08-05 19:18:26 +0300

    Add Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products that you want to use
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iverbs';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyA6-dhQBlmm2ZiDztraMW_rDEwqNHtCa8g",
+      authDomain: "irregularverbs-c54fa.firebaseapp.com",
+      projectId: "irregularverbs-c54fa",
+      storageBucket: "irregularverbs-c54fa.appspot.com",
+      messagingSenderId: "648844572143",
+      appId: "1:648844572143:web:d2fd62366d582cc784b21a"
+    }
+
+    const firebaseApp = initializeApp(firebaseConfig);
+
+    console.log(firebaseApp);
+  }
 }
