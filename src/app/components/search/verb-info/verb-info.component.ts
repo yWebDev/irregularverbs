48017d7292639d22ca4659842d20caf0414fd275
Yurii Shpakovych <yushser@gmail.com>
2024-11-09 14:08:24 +0200

    initial examples layout and component

--- src/app/components/search/verb-info/verb-info.component.ts
+++ src/app/components/search/verb-info/verb-info.component.ts
@@ -1,11 +1,13 @@
 import { Component, input } from '@angular/core';
 import { VerbDetails } from '../../../model/verb-details';
+import { VerbExamplesComponent } from './verb-examples/verb-examples.component';
 
 @Component({
   selector: 'app-verb-info',
   templateUrl: './verb-info.component.html',
   styleUrls: ['./verb-info.component.scss'],
   standalone: true,
+  imports: [VerbExamplesComponent],
 })
 export class VerbInfoComponent {
   details = input<VerbDetails | null>();
