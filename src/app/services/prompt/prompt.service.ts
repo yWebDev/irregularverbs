6254c31f798b5acb3d202e3160a97224be546cf1
Yurii Shpakovych <yushser@gmail.com>
2024-11-24 15:52:42 +0200

    adjust example markup

--- src/app/services/prompt/prompt.service.ts
+++ src/app/services/prompt/prompt.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,18 +9,11 @@ export class PromptService {
   private httpClient = inject(HttpClient);
 
   getVerbExamples(verb: string, form: string): Observable<string> {
-    return this.httpClient
-      .get<string>(`/api/ai/prompt`, {
-        params: {
-          verb,
-          form,
-        },
-      })
-      .pipe(
-        map((res: string) => {
-          console.log(res);
-          return res as string;
-        }),
-      );
+    return this.httpClient.get<string>(`/api/ai/prompt`, {
+      params: {
+        verb,
+        form,
+      },
+    });
   }
 }
