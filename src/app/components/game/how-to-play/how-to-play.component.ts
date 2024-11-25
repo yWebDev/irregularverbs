ed29ea365352156d7cf91987e122d59392a534ba
Yurii Shpakovych <yushser@gmail.com>
2024-11-25 20:02:46 +0200

    add how to play modal

---
+++ src/app/components/game/how-to-play/how-to-play.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { CdkScrollable } from '@angular/cdk/scrolling';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-how-to-play',
+  standalone: true,
+  templateUrl: './how-to-play.component.html',
+  styleUrl: './how-to-play.component.scss',
+  imports: [
+    MatDialogTitle,
+    CdkScrollable,
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose,
+  ],
+})
+export class HowToPlayComponent {}
