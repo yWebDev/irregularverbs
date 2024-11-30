54370793c96a353b36f8e4b477dff11bb4b7d373
Yurii Shpakovych <yushser@gmail.com>
2024-11-30 15:09:13 +0200

    Add donate btn, add meta description, add site info, fix created by link

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { VerbDetails } from '../../model/verb-details';
 import { VerbInputComponent } from './verb-input/verb-input.component';
 import { VerbInfoComponent } from './verb-info/verb-info.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
   standalone: true,
-  imports: [VerbInputComponent, VerbInfoComponent],
+  imports: [VerbInputComponent, VerbInfoComponent, RouterLink],
 })
 export class SearchComponent {
   protected selected: VerbDetails | null = null;
