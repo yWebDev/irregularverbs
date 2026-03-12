de15b0776e7e39122d1eaf56fdc1837b0064e7be
Yurii Shpakovych <yushser@gmail.com>
2026-03-12 12:22:41 +0200

    Remove title test from AppComponent spec and delete AuthorizationGuard spec file to streamline tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,12 +15,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'iverbs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('iverbs');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
